f669f1ea5d7cfa101ebeefe62352b8c48da04d4f
Alex Page <alex.page9005@gmail.com>
2018-10-05 10:49:29 +0300

    Init

---
+++ FPS Platformer/Assets/Scripts/PlayerClimb.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClimb : MonoBehaviour
+{
+    private CharacterController player;
+    private PlayerLook lookScript;
+    private PlayerMove moveScript;
+    private Collider obstacle;
+    private bool isClimbing;
+
+    [SerializeField]
+    private float movementSpeed = 6f;
+
+    [SerializeField]
+    private KeyCode climbKey = KeyCode.Space;
+
+    [SerializeField]
+    private new Camera camera;
+
+    private void Awake()
+    {
+        player = GetComponentInParent<CharacterController>();
+        lookScript = camera.GetComponent<PlayerLook>();
+        moveScript = GetComponentInParent<PlayerMove>();
+    }
+
+    private void Update()
+    {
+        if (IsClimable())
+            StartCoroutine(Climb());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        obstacle = other;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!isClimbing)
+            obstacle = null;
+    }
+
+    private bool IsClimable()
+    {
+        if (!isClimbing &&
+            obstacle != null &&
+            Input.GetKey(climbKey) &&
+            obstacle.bounds.max.y - player.bounds.max.y <= 0.5f)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void ToggleControls(bool isEnabled)
+    {
+        lookScript.enabled = isEnabled;
+        moveScript.IsEnabled = isEnabled;
+        isClimbing = !isEnabled;
+    }
+
+    private IEnumerator Climb()
+    {
+        ToggleControls(false);
+        
+        float currentSpeed = movementSpeed;
+        bool changeSpeed = false;
+
+        Vector3 point = obstacle.ClosestPointOnBounds(player.bounds.max);
+        float halfDistance = (point.y + player.bounds.min.y) / 2;
+
+        while (player.bounds.min.y < point.y)
+        {
+            player.transform.Translate(player.transform.TransformDirection(Vector3.up) * Time.deltaTime * currentSpeed);
+            //player.Move(player.transform.TransformDirection(Vector3.up) * Time.deltaTime * currentSpeed);
+            if (player.bounds.min.y < halfDistance)
+            {
+                camera.transform.Rotate(Vector3.right * Time.deltaTime * 30f * movementSpeed, Space.Self);
+                player.transform.Translate(Vector3.forward * Time.deltaTime * currentSpeed * 0.15f);
+                //player.Move(Vector3.forward * Time.deltaTime * currentSpeed * 0.15f);
+            }
+            else
+            {
+                camera.transform.Rotate(Vector3.left * Time.deltaTime * 30f * currentSpeed, Space.Self);
+                player.transform.Translate(Vector3.forward * Time.deltaTime * currentSpeed * 1.1f);
+                //player.Move(Vector3.forward * Time.deltaTime * currentSpeed * 1.1f);
+            }
+
+            if (!changeSpeed && currentSpeed > movementSpeed / 2)
+                currentSpeed -= movementSpeed * 0.03f;
+            else
+                changeSpeed = true;
+            
+            if (changeSpeed && currentSpeed < movementSpeed * 2)
+                currentSpeed += movementSpeed * 0.015f;
+            
+            yield return null;
+        }
+
+        obstacle = null;
+        ToggleControls(true);
+    }
+}
---
+++ FPS Platformer/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    private string horizontalInputName = "Horizontal";
+
+    [SerializeField]
+    private string verticalInputName = "Vertical";
+
+    [SerializeField]
+    private float movementSpeed = 6f;
+
+    [SerializeField]
+    private float gravity = 20f;
+
+    [SerializeField]
+    private float jumpForce = 8f;
+
+    [SerializeField]
+    private KeyCode jumpKey = KeyCode.Space;
+
+    private float verticalVelocity;
+    private bool hasJumpedTwice;
+
+    private CharacterController charController;
+
+    public bool IsEnabled { get; set; }
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+        IsEnabled = true;
+    }
+
+    private void Update()
+    {
+        if (IsEnabled)
+            PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
+
+        if (charController.isGrounded)
+        {
+            verticalVelocity = -5;
+
+            if (Input.GetKeyDown(jumpKey))
+                verticalVelocity = jumpForce;
+        }
+        else if (!hasJumpedTwice && Input.GetKeyDown(jumpKey))
+            verticalVelocity = jumpForce;
+        else
+        {
+            hasJumpedTwice = false;
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        Vector3 jumpMovement = transform.up * verticalVelocity;
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+        Vector3 moveVector = forwardMovement + rightMovement + jumpMovement;
+
+        charController.Move(moveVector * Time.deltaTime);
+    }
+}
